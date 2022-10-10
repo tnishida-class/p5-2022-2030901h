5760ff972d64815261c51dbcecf3963a3eea4919
2030901h <115509969+2030901h@users.noreply.github.com>
2022-10-11 08:59:38 +0900

    Change section1-1

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -1,16 +1,16 @@
 function setup() {
-  createCanvas(128,128);
+  createCanvas(100,100);
 }
 
 function draw() {
   background(0);
-  strokeWeight(4);
+  strokeWeight(5);
   fill(178, 100, 162);
   triangle(12,120,116,120,116,10);
   fill(94, 185, 84);
   triangle(0,108,106,108,106,0);
   fill(255);
-  textSize(32);
+  textSize(30);
   textFont("serif");
   text("46", 68, 100);
 }
