2f6fdb1ec9f76c727eb049cd4233ebcc68c4779c
2030901h <115509969+2030901h@users.noreply.github.com>
2022-10-25 00:54:15 +0900

    Change section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -12,5 +12,16 @@ function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
   // BLANK[1]
-  ellipse(width / 2, height / 2, size);
+ 
+  if (count<50) {
+    ellipse(width / 2, height / 2, count);
+    
+  }
+  else{
+    ellipse(width / 2, height / 2, 100-count);
+  } 
+
+  if(keyIsDown(UP_ARROW)){count=count*5/4;
+  }
 }
+
